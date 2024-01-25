c35b0311cf9dd68e346d942940cdb10a6ae71720
袁鹏 <rocyuan666@163.com>
2024-01-25 10:46:11 +0800

    init

---
+++ example/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <RocCpn></RocCpn>
+</template>
+
+<script setup lang="ts">
+// 打包前
+import { RocCpn } from '../src/main'
+
+// 打包后
+// import { RocCpn } from '../dist/roc-cli-rollup-vue-template.es'
+// import '../dist/assets/css/roc-cli-rollup-vue-template.css'
+</script>
